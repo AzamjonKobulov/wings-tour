64ee626084eee84c1d26e52d63a04d49f4ba69e9
Azamjon <azamjonkobulov1998@gmail.com>
2024-12-30 16:29:37 +0500

    first commit

---
+++ public/js/script.js
@@ -0,0 +1,62 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const accordionGroups = document.querySelectorAll(".hs-accordion-group");
+
+  accordionGroups.forEach((group) => {
+    const accordions = group.querySelectorAll(".hs-accordion");
+
+    accordions.forEach((accordion) => {
+      const toggleButton = accordion.querySelector(".hs-accordion-toggle");
+      const content = accordion.querySelector(".hs-accordion-content");
+
+      toggleButton.addEventListener("click", () => {
+        const isOpen = accordion.classList.contains("active");
+
+        // Close all other accordions in the group
+        accordions.forEach((otherAccordion) => {
+          if (
+            otherAccordion !== accordion &&
+            otherAccordion.classList.contains("active")
+          ) {
+            const otherContent = otherAccordion.querySelector(
+              ".hs-accordion-content"
+            );
+            otherAccordion.classList.remove("active");
+            otherContent.style.height = otherContent.scrollHeight + "px"; // Set current height to enable smooth transition
+            requestAnimationFrame(() => {
+              otherContent.style.height = "0px";
+            });
+          }
+        });
+
+        if (isOpen) {
+          // Close the clicked accordion
+          accordion.classList.remove("active");
+          content.style.height = content.scrollHeight + "px"; // Set current height to enable smooth transition
+          requestAnimationFrame(() => {
+            content.style.height = "0px";
+          });
+        } else {
+          // Open the clicked accordion
+          accordion.classList.add("active");
+
+          // Set the height explicitly to enable smooth transition
+          content.style.height = content.scrollHeight + "px";
+        }
+      });
+
+      // Set initial height for active accordion
+      if (accordion.classList.contains("active")) {
+        content.style.height = content.scrollHeight + "px";
+      } else {
+        content.style.height = "0px";
+      }
+
+      // Transition end event to reset height property to auto for open accordions
+      content.addEventListener("transitionend", () => {
+        if (accordion.classList.contains("active")) {
+          content.style.height = "auto";
+        }
+      });
+    });
+  });
+});
---
+++ tailwind.config.js
@@ -0,0 +1,66 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      screens: {
+        xs: "480px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1440px",
+      },
+      maxWidth: {
+        base: "86.5rem",
+      },
+      fontFamily: {
+        ubuntu: ["Ubuntu", "sans-serif"],
+      },
+      colors: {
+        brand: {
+          "vivid-purple": {
+            DEFAULT: "#7763E0",
+            pressed: "#453499",
+            hover: "#6451C8",
+            disabled: "#ECE8FF",
+          },
+          "dark-purple": {
+            DEFAULT: "#382176",
+            pressed: "#331F6A",
+            hover: "#6A5B95",
+            disabled: "#DAD5E8",
+          },
+          liliac: {
+            DEFAULT: "#D9DCFC",
+            light: "#F5F3FF",
+            pressed: "#9AA1EF",
+            hover: "#BBC0F6",
+            disabled: "#EDEFFC",
+          },
+          neutrals: {
+            "faded-puple": "#6B617E",
+            "dirty-liliac": "#B9B1C9",
+            gray: "#CBCBD1",
+          },
+          system: {
+            red: "#FF4209",
+            blue: "#42ECFF",
+            green: "#5CBB40",
+          },
+        },
+      },
+      spacing: {
+        72: "72px",
+        54: "54",
+      },
+      borderRadius: {
+        10: "10px",
+      },
+      backgroundImage: {
+        services: "linear-gradient(180deg, #FFFFFF 0%, #DBD5FB 100%);",
+      },
+    },
+  },
+  plugins: [],
+};
